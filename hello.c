4ea198e46f66055601b1bdc44cddca3a987cc472
Kun-Yu Lee <klee39@huskers.unl.edu>
2021-08-26 18:01:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Chris Bourke
+ *         cbourke@cse.unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kun-Yu Lee\n");
+  printf("Kun-Yu Lee\n");
+
+  return 0;
+}
